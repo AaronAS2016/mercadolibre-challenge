9b9493cf4d80c1dc8f26a5ec2f55b931b417de36
Aaron Saban <aaronsaban.as2012@gmail.com>
2021-04-17 15:40:59 -0300

    SERVER: added search items endpoint

--- server/src/routes/items.routes.ts
+++ server/src/routes/items.routes.ts
@@ -1,9 +1,17 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { getItems } from "../controllers/items";
 
 const itemsRouter = Router();
 
-itemsRouter.get('/', (request, response) => {
-  return response.json("OK");
+itemsRouter.get("/", async (request, response) => {
+  try { 
+    const query = String(request.query.q);
+    const res = await getItems(query);
+    return response.status(200).send({ res })
+  }catch (error) {
+    console.log(error);
+    response.send({ error: error });
+  }
 });
 
 export default itemsRouter;
