474b357286150a5da41649cf3a812a015e6e954a
Aaron Saban <aaronsaban.as2012@gmail.com>
2021-04-17 10:12:21 -0300

    ADD: basic styles for searchbar

--- client/src/components/logo/logo.tsx
+++ client/src/components/logo/logo.tsx
@@ -4,8 +4,8 @@ import MercadoLibreLogo from 'assets/img/Logo_ML@2x.png';
 
 export const Logo:React.FC = () => {
     return(
-        <picture>
-            <img src={ MercadoLibreLogo } />
+        <picture className="logo-wrapper">
+            <img className="logo_wrapper__image" src={ MercadoLibreLogo } alt="MercadoLibre" />
         </picture>
     )
 }
--- client/src/components/searchbar/searchbar.tsx
+++ client/src/components/searchbar/searchbar.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import "./searchbar.scss"
+import React, { useState } from "react";
+import "./searchbar.scss";
 
-export const SearchBar:React.FC = () => {
-    return(
-        <nav>
-            
-        </nav>
-    )
-}
+export const SearchBar: React.FC = () => {
+  const [text, setText] = useState("");
+
+  return (
+    <nav className="navbar-wrapper">
+      <input
+        className="navbar-wrapper__search_input"
+        placeholder="Nunca dejes de buscar"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <button className="navbar-wrapper__search_button" />
+    </nav>
+  );
+};
