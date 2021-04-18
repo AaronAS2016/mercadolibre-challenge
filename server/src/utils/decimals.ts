da5339072fc157e8c179e289668a9e994c09e294
Aaron Saban <aaronsaban.as2012@gmail.com>
2021-04-18 12:42:14 -0300

    SERVER: basic setup for jest

--- server/src/utils/decimals.ts
+++ server/src/utils/decimals.ts
@@ -1,5 +1,5 @@
   
-  export const countDecimals = (value: number) => {
+  export const countDecimals = (value: number | null | undefined): number | null => {
     if (!value) return null;
     if (Math.floor(value) === value) return 0;
     return value.toString().split(".")[1].length || 0;
