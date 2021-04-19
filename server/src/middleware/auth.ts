35caa96c02ba3ca29562046048871a7cfcbddaa9
Aaron Saban <aaronsaban.as2012@gmail.com>
2021-04-18 23:44:41 -0300

    REFACTOR: moved magic number to constant

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,14 +1,15 @@
+import { CLIENT_FORBIDDEN_STATUS } from './../constant/http_status_codes';
 import { author } from "../constant";
 
 export const auth = (req:any, res:any, next:any) => {
     const sign = req.headers.sign;
     const parsedAuthor = JSON.stringify(author);
-    if(!sign ) res.status(403).send({message: "Not signature sended" });
+    if(!sign ) res.status(CLIENT_FORBIDDEN_STATUS).send({message: "Not signature sended" });
 
     if(sign == parsedAuthor ){
         next();
     }else{
-        res.status(403).send({message: "invalid signature" });
+        res.status(CLIENT_FORBIDDEN_STATUS).send({message: "invalid signature" });
     }
 
 }
