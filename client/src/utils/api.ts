5e217cb5711b3f007428e8c59fb218fab38c7134
Aaron Saban <aaronsaban.as2012@gmail.com>
2021-04-19 08:52:31 -0300

    CLIENT: added error handlers

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,22 +1,28 @@
-import { headers } from './../constants/api';
+import { headers } from "./../constants/api";
 import { API_SEARCH_ITEM, API_SEARCH_ITEMS } from "~/constants/api";
 
-
 export const getRequest = async (url: string, options: any = {}) => {
-    const req = await fetch(url, {...options});
-    const data = await req.json();
-    return data;
-}
-
+  const req = await fetch(url, { ...options });
+  const data = await req.json();
+  return data;
+};
 
 export const getItems = async (query: string | null | undefined) => {
-    if (!query) { return []; }
+  if (!query) {
+    return [];
+  }
+  try {
     const url = API_SEARCH_ITEMS + query;
-    return await getRequest(url, headers); 
-}
+    return await getRequest(url, headers);
+  } catch (error) {
+    return () => {throw new Error(error) };
+  }
+};
 
 export const getItem = async (id: string | null | undefined) => {
-    if (!id) { return []; }
-    const url = API_SEARCH_ITEM + id;
-    return await getRequest(url, headers); 
-}
+  if (!id) {
+    return [];
+  }
+  const url = API_SEARCH_ITEM + id;
+  return await getRequest(url, headers);
+};
